7301ce3a7bdb35d8a736c443d76833eb2dbd0de8
Aya <ayabonga@projectcodex.co>
2018-06-17 17:47:45 +0200

    Define: Component: Menu Item

---
+++ src/components/menuItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import * as R from 'ramda';
+
+class MenuItem extends Component {
+    constructor(){
+        super();
+        this.state = { kidsState:'hidden' }
+        this.toggleKidsState = this.toggleKidsState.bind(this);
+    }
+
+    toggleKidsState(){
+        console.log('toggilng kid state')
+        if(this.state.kidsState === 'hidden'){
+            this.setState({kidsState: 'visible'});
+        }
+        else{
+            this.setState({kidsState: 'hidden'});
+        }
+        
+    }
+
+    render() {
+      const {header, children , run, body} = this.props;
+      let onclick = !R.isNil(children) ? {} :  run;
+      return (
+        <div className='menu-item'>
+            <div className='menu-item-header' onClick={R.isEmpty(onclick) ? this.toggleKidsState : onclick}>{header}</div>
+            <div className={`menu-item-body ${this.state.kidsState}`}>{children}</div>
+        </div>
+      );
+    }
+  }
+
+MenuItem.propTypes ={
+    header: PropTypes.string.isRequired,
+    run: PropTypes.func,
+};
+
+export default MenuItem;
+
