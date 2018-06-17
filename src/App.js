c88a56763eb21bc31593dce0f3e77c90cb3d553c
Aya <ayabonga@projectcodex.co>
2018-06-17 17:48:45 +0200

    Refactor: Use Component grid view

--- src/App.js
+++ src/App.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
+import { List, Grid, Image } from 'semantic-ui-react'
+import MenuTree from './components/menuTree'
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = { mainView :'' }
+    this.configureSSH = this.configureSSH.bind(this);
+  }
+
+  configureSSH(){
+    const mainView = (
+      <div> 
+        <h1> SSH Configuration Settings </h1>
+        <h3> Configure SSH authorized keys below </h3>
+      </div>
+    )
+    this.setState({ mainView })
+  }
+
   render() {
+    const options= [
+      {
+        type:'menu-item',
+        header:'Dashboard'
+      },
+      {
+        type:'menu-item-with-body',
+        header:'Settings',
+        run: ()=> alert('hello from settings'),
+        body:[
+          {
+            type:'menu-item-with-body',
+            header:"Security",
+            run: ()=> alert('hello from security'),
+            body:[
+              {
+                type:'menu-item',
+                header: 'SSH',
+                run: this.configureSSH,
+              }
+            ]
+          }
+        ]
+      },
+      {
+        type:'menu-item',
+        header:'New'
+      }]
+
+    
+    const { mainView } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Grid className='full-screen-grid'>
+          <Grid.Row>
+            <Grid.Column width={3} className='nav'>
+              <h2>Options</h2>
+              <MenuTree items={options} />
+            </Grid.Column>
+            <Grid.Column width={13} className='control-container'>
+              {mainView}
+            </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
     );
   }
 }
