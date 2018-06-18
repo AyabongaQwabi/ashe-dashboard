3668bd14b3bbdcaa5f3f5eb8ecf7e8d87d2da7a3
Aya <ayabonga@projectcodex.co>
2018-06-18 11:14:30 +0200

    Add: Home component

---
+++ src/views/home.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+
+class home extends Component {
+    render() {
+       
+        return (
+           <div className='ssh-view'> 
+                <h1> Ashe Dashboard </h1>
+            </div>
+        )
+    }
+}
+
+
+export default home;
