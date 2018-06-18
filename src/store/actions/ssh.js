d6254622ea90c16860461811c7a06079675e0c9f
Aya <ayabonga@projectcodex.co>
2018-06-18 22:59:15 +0200

    Feat: Add: "AddNew Key" functionality

--- src/store/actions/ssh.js
+++ src/store/actions/ssh.js
@@ -1,3 +1,10 @@
+export const addKey = (dispatch) => (key) =>{
+    return dispatch({
+        type:'SSH_ADD_KEY',
+        payload:key,
+    })
+}
+
 export default () => ({
         type:'DEFAULT',
         payload: null,
--- src/views/ssh.js
+++ src/views/ssh.js
@@ -1,15 +1,65 @@
 
 import React, { Component } from 'react';
+import { Button, Icon } from 'semantic-ui-react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addKey } from '../store/actions/ssh'
 
 class sshView extends Component {
+    constructor(){
+        super();
+        this.addNewKey = this.addNewKey.bind(this);
+        this.updateKeyArea= this.updateKeyArea.bind(this);
+        this.state = {
+            newKey: {
+                addNewKeyActive: false,
+                keyArea:'',
+            }
+        }
+    }
+    updateKeyArea(e){
+        const keyArea = e.target.value;
+        console.log(e.target.value)
+        const  { addNewKeyActive } = this.state.newKey;
+        this.setState({ newKey: { addNewKeyActive, keyArea } })
+    }
+
+    addNewKey() {
+        const { keyArea } = this.state.newKey;
+        const addNewKeyActive = !this.state.newKey.addNewKeyActive;
+        this.setState({ newKey: { addNewKeyActive, keyArea } })
+    }
+
     render() {
-        const { keys } = this.props;
+        const { keys, addKey } = this.props;
+        const addSSHKey = (k) => () => addKey(k);
+
+        const { addNewKeyActive ,keyArea } = this.state.newKey;
         return (
            <div className='ssh-view'> 
                 <h1> SSH Configuration Settings </h1>
                 <div className='ssh-view-box'>
                 <h3> Configure SSH settings below </h3>
                 <h4> These are the SSH keys allowed to connect to this computer </h4>
+                <div className='action-tray'>
+                    <Button animated='vertical' onClick={this.addNewKey}>
+                        <Button.Content hidden >Add</Button.Content>
+                        <Button.Content visible>
+                            <Icon name='plus' />
+                        </Button.Content>
+                    </Button>
+                    <div className='action-playground'>
+                        {addNewKeyActive && 
+                            <div>
+                                <p> Enter/paste key here </p>
+                                <textarea value={keyArea} onChange={this.updateKeyArea}></textarea>
+                                <Button color='purple' onClick={addSSHKey(keyArea)}>
+                                    Add
+                                </Button>
+                            </div>
+                        }
+                    </div>
+                </div>
                 <div className='keys'> 
                     {keys.map((key) => (
                     <div className='ssh-box'>
@@ -24,5 +74,11 @@ class sshView extends Component {
     }
 }
 
+sshView.propTypes = {
+    keys: PropTypes.array,
+}
 
-export default sshView;
+const mapDispatchToProps = (dispatch) => ({
+    addKey: addKey(dispatch),
+})
+export default connect(null, mapDispatchToProps)(sshView)
