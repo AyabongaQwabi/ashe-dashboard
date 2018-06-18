7826cf101ba910a57138197c08f558ee807332c6
Aya <ayabonga@projectcodex.co>
2018-06-18 07:30:03 +0200

    Refactor: Decouple App component

--- src/App.js
+++ src/App.js
@@ -1,82 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './style/App.css';
-import { List, Grid, Image } from 'semantic-ui-react'
-import MenuTree from './components/menuTree'
 
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react'
+import nav from './config/nav.json'
+import Nav from './views/nav';
+import './style/App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state = { mainView :'' }
-    this.configureSSH = this.configureSSH.bind(this);
-  }
-
-  configureSSH(){
-    const mainView = (
-      <div> 
-        <h1> SSH Configuration Settings </h1>
-        <h3> Configure SSH authorized keys below </h3>
-      </div>
-    )
-    this.setState({ mainView })
   }
 
   render() {
-    const options= [
-      {
-        type:'menu-item',
-        header:'Dashboard',
-        icon:'fa fa-chart-bar',
-      },
-      {
-        type:'menu-item',
-        icon:'fa fa-history',
-        header:'Automation'
-      },
-      {
-        type:'menu-item-with-body',
-        header:'Settings',
-        icon:'fa fa-cog',
-        run: ()=> alert('hello from settings'),
-        body:[
-          {
-            icon: 'fa fa-lock',
-            type:'menu-item-with-body',
-            header:"Security",
-            run: ()=> alert('hello from security'),
-            body:[
-              {
-                icon:'fa fa-key',
-                type:'menu-item',
-                header: 'SSH',
-                run: this.configureSSH,
-              }
-            ]
-          }
-        ]
-      },
-      {
-        type:'menu-item-with-body',
-        header:'Users',
-        icon:'fa fa-user',
-        body:[
-          {
-            icon:'fa fa-user-cog',
-            type:'menu-item',
-            header: 'Manage',
-            run: this.configureSSH,
-          },
-          {
-            icon:'fas fa-folder-open',
-            type:'menu-item',
-            header: 'Activity',
-            run: ()=>{},
-          }
-        ]
-      }]
-
-    
     const { mainView } = this.state;
     return (
       <div className="App">
@@ -84,7 +19,7 @@ class App extends Component {
           <Grid.Row>
             <Grid.Column width={3} className='nav'>
               <h2>Options</h2>
-              <MenuTree items={options} />
+              <Nav items={nav}/>
             </Grid.Column>
             <Grid.Column width={13} className='control-container'>
               {mainView}
